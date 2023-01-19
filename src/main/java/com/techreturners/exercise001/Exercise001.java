d823004ea0bd6d37558c6003a50d445a3dc55dba
Vasanthi Vaithiyanathan <Vasanthi@preventx.com>
2023-01-19 11:48:03 +0000

    Exercise001 done, please review.Thanks.

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,41 @@
 package com.techreturners.exercise001;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise001 {
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        return (firstName.substring(0, 1) + "." + lastName.substring(0, 1));
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        return Double.parseDouble(df.format((originalPrice * vatRate)/100 + originalPrice));
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        String reverseStr = "";
+        for (int i = (sentence.length() - 1); i >= 0; i--) {
+            reverseStr = reverseStr + sentence.charAt(i);
+        }
+        return reverseStr;
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        List<User> linuxUsers = new ArrayList<User>();
+
+        for (User linusers : users ){
+            if (linusers.getType() =="Linux"){
+                linuxUsers.add(linusers);
+            }
+        }
+
+        return linuxUsers.size();
     }
 }
