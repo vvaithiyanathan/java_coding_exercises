387f140af3f14444c2386a0838d0ff02b4227669
Vasanthi Vaithiyanathan <Vasanthi@preventx.com>
2023-01-21 10:56:33 +0000

    Exercise 4, please review. Thanks

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,55 @@
 package com.techreturners.exercise005;
+public class Exercise005
+{
 
-public class Exercise005 {
+    static int size = 26;
+    static boolean isLetter(char ch)
+    {
+        if (!Character.isLetter(ch))
+            return false;
 
-    public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        return true;
     }
+    public boolean isPangram(String input)
+    {
+        // Convert the given string
+        // into lowercase
 
+        if (input.isEmpty() ) {
+            return false;
+        }else{
+        input = input.toLowerCase();
+
+        // Create a frequency array to
+        // mark the present letters
+        boolean[] present = new boolean[size];
+
+        // Traverse for each character
+        // of the string
+        for (int i = 0; i < input.length(); i++) {
+
+            // If the current character
+            // is a letter
+            if (isLetter(input.charAt(i))) {
+
+                // Mark current letter as present
+                int letter = input.charAt(i) - 'a';
+                present[letter] = true;
+            }
+        }
+
+        // Traverse for every letter
+        // from a to z
+        for (int i = 0; i < size; i++) {
+
+            // If the current character
+            // is not present in string
+            // then return false,
+            // otherwise return true
+            if (!present[i])
+                return false;
+        }
+        return true;
+    }
+    }
 }
